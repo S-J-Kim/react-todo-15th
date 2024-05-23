24f2b136a97b3557a23a8ff52245ed7b70de7f87
Seon-Jong Kim <30287999+S-J-Kim@users.noreply.github.com>
2024-05-23 16:31:19 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
       <TodoItemContainer type={'To-Do'} items={getUndoneItems()} key={'todo'} />
       <TodoItemContainer type={'Done'} items={getDoneItems()} key={'done'} />
       <InputBox />
+      <img src='https://oscar-apprunner-demo.s3.amazonaws.com/github.png'/>
     </>
   );
 }
